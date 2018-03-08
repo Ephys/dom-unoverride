6e4de36308a83441d79ac068efa22c27e3f1ff5d
Guylian Cox <gc@pictawall.com>
2018-03-08 15:20:39 +0100

    build: Transpile to ES5

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+import babel from 'rollup-plugin-babel';
+
 export default {
   input: 'src/index.js',
   output: [{
@@ -8,4 +10,9 @@ export default {
     file: 'dist/bundle.mjs',
     format: 'es',
   }],
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+    }),
+  ],
 };
